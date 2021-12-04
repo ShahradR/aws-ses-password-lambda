c034de1447defde82e8bf74542cab13af9a6aec1
Shahrad Rezaei <shahrad@rezaei.io>
2021-12-04 05:25:48 +0000

    feat: implement SES password generation logic
    Implement the SES password generation logic using the pseudocode and
    example Python implementation made available at https://go.aws/3rE70XN.
    
    To help write the unit tests, we pass the example secret access key
    provided in the docs to the Python script, and validate that both our
    Lambda function and the Python script return the same value.
    
    This also adds the "NoEcho" parameter in the response, to make sure that
    the SES password is masked when making API calls or retrieving the value
    through functions like Fn::GetAtt, for example.

--- lambda/ses-password-lambda/src/index.ts
+++ lambda/ses-password-lambda/src/index.ts
@@ -3,12 +3,61 @@ import {
   CloudFormationCustomResourceResponse,
 } from "aws-lambda";
 
+import * as crypto from "crypto";
+
 var aws = require("aws-sdk");
 
 export const handler = async (
   event: CloudFormationCustomResourceEvent
 ): Promise<CloudFormationCustomResourceResponse> => {
-  console.log("Entered the application code");
+  const secretAccessKey = event.ResourceProperties.AWSSecretAccessKey;
+  let awsRegion: string = process.env.AWS_REGION!;
+
+  if (awsRegion === undefined || awsRegion === "" || awsRegion === null) {
+    console.error("The AWS_REGION environment variable has not been set");
+    return {
+      PhysicalResourceId: "",
+      Data: {},
+      LogicalResourceId: event.LogicalResourceId,
+      RequestId: event.RequestId,
+      Status: "FAILED",
+      Reason: "The AWS_REGION environment variable has not been set",
+      StackId: event.StackId,
+    };
+  }
+
+  const date: string = "11111111";
+  const service = "ses";
+  const terminal = "aws4_request";
+  const message = "SendRawEmail";
+  const version = Buffer.from([0x04]);
+
+  let kDate: Buffer = crypto
+    .createHmac(
+      "sha256",
+      Buffer.from(`AWS4${secretAccessKey}`).toString("utf8")
+    )
+    .update(date)
+    .digest();
+  let kRegion: Buffer = crypto
+    .createHmac("sha256", kDate)
+    .update(awsRegion)
+    .digest();
+  let kService: Buffer = crypto
+    .createHmac("sha256", kRegion)
+    .update(service)
+    .digest();
+  let kTerminal: Buffer = crypto
+    .createHmac("sha256", kService)
+    .update(terminal)
+    .digest();
+  let kMessage: Buffer = crypto
+    .createHmac("sha256", kTerminal)
+    .update(message)
+    .digest();
+
+  let signatureAndVersion = Buffer.concat([version, kMessage]);
+  let smtpPassword = Buffer.from(signatureAndVersion).toString("base64");
 
   const responseData: CloudFormationCustomResourceResponse = {
     Status: "SUCCESS",
@@ -18,8 +67,9 @@ export const handler = async (
     RequestId: "request-id",
     LogicalResourceId: "test-resource",
     Data: {
-      TestProperty: "test-value",
+      SMTPPassword: smtpPassword,
     },
+    NoEcho: true,
   };
 
   return Promise.resolve(responseData);
--- lambda/ses-password-lambda/test/index.test.ts
+++ lambda/ses-password-lambda/test/index.test.ts
@@ -17,11 +17,13 @@ describe("the handler function", () => {
       ResourceProperties: {
         ServiceToken:
           "arn:aws:lambda:us-east-1:123456789012:function:test-function",
-        TestProperty: "test-value",
+        AWSSecretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
       },
       ServiceToken:
         "arn:aws:lambda:us-east-1:123456789012:function:test-function",
     };
+
+    process.env.AWS_REGION = "ca-central-1";
 
     const cfnResponse: CloudFormationCustomResourceResponse = {
       Status: "SUCCESS",
@@ -31,12 +33,99 @@ describe("the handler function", () => {
       RequestId: "request-id",
       LogicalResourceId: "test-resource",
       Data: {
-        TestProperty: "test-value",
+        SMTPPassword: "BAasSsnWzpnb1x/UOD58KEOQvGKrh4RNYWJDqqq9/dFC",
       },
+      NoEcho: true,
     };
 
     let result: CloudFormationCustomResourceResponse = await handler(cfnEvent);
 
     expect(cfnResponse).toEqual(result);
+  });
+
+  it("should derive the SMTP password from the AWS access key ID", async () => {
+    const cfnEvent: CloudFormationCustomResourceEvent = {
+      RequestType: "Create",
+      ResponseURL: "https://cloudformation-custom-resource-response-url",
+      StackId:
+        "arn:aws:cloudformation:us-east-1:123456789012:stack/test-stack/guid",
+      RequestId: "request-id",
+      LogicalResourceId: "test-resource",
+      ResourceType: "Custom::Test",
+      ResourceProperties: {
+        ServiceToken:
+          "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+        AWSSecretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+      },
+      ServiceToken:
+        "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+    };
+    process.env.AWS_REGION = "ca-central-1";
+
+    let result: CloudFormationCustomResourceResponse = await handler(cfnEvent);
+
+    expect(result.Data!.SMTPPassword).toEqual(
+      "BAasSsnWzpnb1x/UOD58KEOQvGKrh4RNYWJDqqq9/dFC"
+    );
+  });
+
+  it("should return an error if the AWS_ACCOUNT_ID value has not been set", async () => {
+    const cfnEvent: CloudFormationCustomResourceEvent = {
+      RequestType: "Create",
+      ResponseURL: "https://cloudformation-custom-resource-response-url",
+      StackId:
+        "arn:aws:cloudformation:us-east-1:123456789012:stack/test-stack/guid",
+      RequestId: "request-id",
+      LogicalResourceId: "test-resource",
+      ResourceType: "Custom::Test",
+      ResourceProperties: {
+        ServiceToken:
+          "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+        AWSSecretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+      },
+      ServiceToken:
+        "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+    };
+
+    process.env.AWS_REGION = "";
+
+    const cfnResponse: CloudFormationCustomResourceResponse = {
+      Status: "FAILED",
+      PhysicalResourceId: "",
+      StackId:
+        "arn:aws:cloudformation:us-east-1:123456789012:stack/test-stack/guid",
+      RequestId: "request-id",
+      LogicalResourceId: "test-resource",
+      Data: {},
+      Reason: "The AWS_REGION environment variable has not been set",
+    };
+
+    let result: CloudFormationCustomResourceResponse = await handler(cfnEvent);
+
+    expect(cfnResponse).toEqual(result);
+  });
+  it("uses the NoEcho field to mask the output of the custom resource", async () => {
+    const cfnEvent: CloudFormationCustomResourceEvent = {
+      RequestType: "Create",
+      ResponseURL: "https://cloudformation-custom-resource-response-url",
+      StackId:
+        "arn:aws:cloudformation:us-east-1:123456789012:stack/test-stack/guid",
+      RequestId: "request-id",
+      LogicalResourceId: "test-resource",
+      ResourceType: "Custom::Test",
+      ResourceProperties: {
+        ServiceToken:
+          "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+        AWSSecretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+      },
+      ServiceToken:
+        "arn:aws:lambda:us-east-1:123456789012:function:test-function",
+    };
+
+    process.env.AWS_REGION = "ca-central-1";
+
+    let result: CloudFormationCustomResourceResponse = await handler(cfnEvent);
+
+    expect(result.NoEcho!).toEqual(true);
   });
 });
